f1608d1ace8016233f77f07b268f85bb96a1e6ff
RJ Marsan <rjmarsan@gmail.com>
2012-05-02 22:15:32 -0500

    SKDJF stuff looks good adding params

--- static/new/js/synth.js
+++ static/new/js/synth.js
@@ -55,74 +55,98 @@ function playExample() {
     }
     extend(Drum, AudioletGroup);
 
-    var audiolet = new Audiolet();
-    var synth = new Synth(audiolet);
-    var bass = new Synth(audiolet);
+
+    var Instruments = function(audiolet, url) {
+        AudioletGroup.apply(this, [audiolet, 0, 1]);
+
+        this.synth = new Synth(audiolet);
+        this.bass = new Synth(audiolet);
+        this.bd = new Drum(audiolet, 'audio/bd_stereo.wav');
+        this.sn = new Drum(audiolet, 'audio/sn_stereo.wav');
+        this.hh = new Drum(audiolet, 'audio/hh_stereo.wav');
 
 
-    //drumsss
-    // Create empty buffers for the bass drum, hi hat and snare drum
-    var bd = new Drum(audiolet, 'audio/bd_stereo.wav');
-    var sn = new Drum(audiolet, 'audio/sn_stereo.wav');
-    var hh = new Drum(audiolet, 'audio/hh_stereo.wav');
-
-
-
-
-    var synthFrequencyPattern = new PSequence([70, 66, 72, 66, 75, 66, 77, -1, 80, -1, 90, -1],Infinity);
-    var bassFrequencyPattern = new PSequence([30, -1, 32, -1, 35, -1, 37, -1, 30, -1, 30, -1],Infinity);
-    var kickPattern = new PSequence([1, 0, 0, 0],Infinity);
-    var snarePattern = new PSequence([0, 1, 0, 1],Infinity);
-    var hhPattern = new PSequence([1, 1, 1, 1],Infinity);
-    var synthDurationPattern = new PSequence([0.9, 0.1], Infinity);
-    var bassDurationPattern = new PSequence([0.9, 0.1], Infinity);
-    var schedulerFunction = function(freq, gate) {
-            if (freq >= 0) {
-                this.saw.frequency.setValue(mtof(freq));
-                this.env.gate.setValue(1);
-            } else {
-                this.env.gate.setValue(0);
-            }
+        this.synthFrequencyPattern = new PSequence([70, 66, 72, 66, 75, 66, 77, -1, 80, -1, 90, -1],Infinity);
+        this.bassFrequencyPattern = new PSequence([30, -1, 32, -1, 35, -1, 37, -1, 30, -1, 30, -1],Infinity);
+        this.kickPattern = new PSequence([1, 0, 0, 0],Infinity);
+        this.snarePattern = new PSequence([0, 1, 0, 1],Infinity);
+        this.hhPattern = new PSequence([1, 1, 1, 1],Infinity);
+        this.synthDurationPattern = new PSequence([0.9, 0.1], Infinity);
+        this.bassDurationPattern = new PSequence([0.9, 0.1], Infinity);
+        this.schedulerFunction = function(freq, gate) {
+                if (freq >= 0) {
+                    this.saw.frequency.setValue(mtof(freq));
+                    this.env.gate.setValue(1);
+                } else {
+                    this.env.gate.setValue(0);
+                }
+            };
+        this.drumSchedulerFunction = function(volume) {
+            this.gain.gain.setValue(volume);
+            this.trigger.trigger.setValue(1);
         };
-    var drumSchedulerFunction = function(volume) {
-        this.gain.gain.setValue(volume);
-        this.trigger.trigger.setValue(1);
-    };
-    audiolet.scheduler.play([synthFrequencyPattern], synthDurationPattern, schedulerFunction.bind(synth));
-    audiolet.scheduler.play([bassFrequencyPattern], bassDurationPattern, schedulerFunction.bind(bass));
-    audiolet.scheduler.play([kickPattern], 0.5, drumSchedulerFunction.bind(bd));
-    audiolet.scheduler.play([snarePattern], 0.5, drumSchedulerFunction.bind(sn));
-    audiolet.scheduler.play([hhPattern], 0.5, drumSchedulerFunction.bind(hh));
-    hh.finalgain.gain.setValue(0.5);
+        audiolet.scheduler.play([this.synthFrequencyPattern], this.synthDurationPattern, this.schedulerFunction.bind(this.synth));
+        audiolet.scheduler.play([this.bassFrequencyPattern], this.bassDurationPattern, this.schedulerFunction.bind(this.bass));
+        audiolet.scheduler.play([this.kickPattern], 0.5, this.drumSchedulerFunction.bind(this.bd));
+        audiolet.scheduler.play([this.snarePattern], 0.5, this.drumSchedulerFunction.bind(this.sn));
+        audiolet.scheduler.play([this.hhPattern], 0.5, this.drumSchedulerFunction.bind(this.hh));
+        this.hh.finalgain.gain.setValue(0.5);
 
 
 
 
-    // The rest of the pipeline
-    var mainEffectsFeed = new Gain(audiolet);
-    var preeffectsGain = new Gain(audiolet, 0.6);
-    var bitcrush = new BitCrusher(audiolet, 5);
-    var reverb = new Reverb(audiolet, 0.5, 1, 0.3);
-    var posteffectsGain = new Gain(audiolet, 1.0);
-    synth.connect(mainEffectsFeed);
-    bass.connect(mainEffectsFeed);
-    bd.connect(mainEffectsFeed);
-    sn.connect(mainEffectsFeed);
-    hh.connect(mainEffectsFeed);
-    mainEffectsFeed.connect(preeffectsGain);
-    preeffectsGain.connect(bitcrush);
-    bitcrush.connect(reverb);
-    reverb.connect(posteffectsGain);
-    posteffectsGain.connect(audiolet.output);
+        // The rest of the pipeline
+        this.mainEffectsFeed = new Gain(audiolet);
+        this.preeffectsGain = new Gain(audiolet, 0.6);
+        this.bitcrush = new BitCrusher(audiolet, 5);
+        this.reverb = new Reverb(audiolet, 0.5, 1, 0.3);
+        this.posteffectsGain = new Gain(audiolet, 1.0);
+        this.synth.connect(this.mainEffectsFeed);
+        this.bass.connect(this.mainEffectsFeed);
+        this.bd.connect(this.mainEffectsFeed);
+        this.sn.connect(this.mainEffectsFeed);
+        this.hh.connect(this.mainEffectsFeed);
+        this.mainEffectsFeed.connect(this.preeffectsGain);
+        this.preeffectsGain.connect(this.bitcrush);
+        this.bitcrush.connect(this.reverb);
+        this.reverb.connect(this.posteffectsGain);
+        this.posteffectsGain.connect(this.outputs[0]);
 
-
-    setReverbMix = function(value) {
-        reverb.mix.setValue(value);
     }
+    extend(Instruments, AudioletGroup);
+    audiolet = new Audiolet();
+    synth = new Instruments(audiolet);
+
+    synth.connect(audiolet.output);
+
+
 };
 
+var synth;
+var audiolet;
 
-var setReverbMix;
+function clamp(low, high,  value) { return Math.max(low, Math.min(high, value)) };
+
+function setReverbMix(value) { synth.reverb.mix.setValue(clamp(0,1,value)); }
+function setReverbRoom(value) { synth.reverb.roomSize.setValue(value); }
+function setReverbDamping(value) { synth.reverb.damping.setValue(value); }
+function setBitcrushBits(value) { synth.bitcrush.bits.setValue(value); }
+function setPreEffectsVol(value) { synth.preeffectsGain.gain.setValue(value); }
+function setPostEffectsVol(value) { synth.posteffectsGain.gain.setValue(value); }
+function setTempo(value) { audiolet.scheduler.setTempo(value); }
+
+
+
+var parameters = {
+   preeffectsvol: {set:setPreEffectsVol, name:"preeffectsvol", title:"Pre Effects Volume", max:1.0, min:0.0, curve:"linear", units:"", category:"effects"},
+   reverbmix: {set:setReverbMix, name:"reverbmix", title:"Reverb Mix", max:1.0, min:0.0, curve:"linear", units:"", category:"effects"},
+   reverbroom: {set:setReverbRoom, name:"reverbroom", title:"Reverb Room", max:1.0, min:0.0, curve:"linear", units:"", category:"effects"},
+   reverbdamping: {set:setReverbDamping, name:"reverbdamping", title:"Reverb Damping", max:1.0, min:0.0, curve:"linear", units:"", category:"effects"},
+   bitcrushbits: {set:setBitcrushBits, name:"bitcrushbits", title:"Bitcrush", max:32.0, min:1.0, curve:"linear", units:"bits", category:"effects"},
+   posteffectsvol: {set:setPostEffectsVol, name:"posteffectsvol", title:"Post Effects Volume", max:1.0, min:0.0, curve:"linear", units:"", category:"main"},
+   tempo: {set:setTempo, name:"tempo", title:"Tempo", max:300.0, min:20.0, curve:"linear", units:"bpm", category:"main"},
+}
+
 
 
 function mtof(midi) {
